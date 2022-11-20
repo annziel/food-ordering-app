013f732311eee65f136868d32bcabe56f7cd5590
Anna Zielinska <115597522+annziel@users.noreply.github.com>
2022-11-20 19:17:00 +0000

    Render the menu (with JS)

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 import { createMenuArray } from "./data.js";
 
-const menu = createMenuArray()
+const menuData = createMenuArray()
+
+function render() {
+    // console.log(menuData)
+    let menuHtml = ""
+    menuData.forEach(function(item){
+        menuHtml += `
+        <div class="menu-item">
+            <p class="item-emoji">${item.emoji}</p>
+            <div class="item-description">
+                <p class="item-name">${item.name}</p>
+                <p class="item-ingredients">${item.ingredients.join(", ")}</p>
+                <p class="item-price">$${item.price}</p>
+            </div>
+            <button class="add-item">+</button>
+        </div>
+            `
+    document.getElementById("menu-list").innerHTML = menuHtml
+    })
+}
+
+render()
