ad159c54da8b5ad7cef77df4cf85194823e8a856
Anna Zielinska <115597522+annziel@users.noreply.github.com>
2022-11-20 22:11:56 +0000

    Add basic order summery and addItem eventListener

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ import { createMenuArray } from "./data.js";
 
 const menuData = createMenuArray()
 
+document.addEventListener("click", function(e){
+    if (e.target.dataset.addItem) {
+        addItem()
+    }
+})
+
+function addItem(itemId) {
+    console.log("added")
+}
+
+
 function render() {
     let menuHtml = ""
     menuData.forEach(function(item){
@@ -13,7 +24,7 @@ function render() {
                 <p class="item-ingredients">${item.ingredients.join(", ")}</p>
                 <p class="item-price">$${item.price}</p>
             </div>
-            <button class="add-item">+</button>
+            <button class="add-item" data-add-item="${item.id}">+</button>
         </div>
             `
     document.getElementById("menu-list").innerHTML = menuHtml
